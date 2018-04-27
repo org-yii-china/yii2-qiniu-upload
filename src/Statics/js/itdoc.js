9e11733b088c11b8c0036f573cf0381f5d01a4a4
huangxianan <hzhxn@tairanchina.com>
2018-04-27 15:02:47 +0800

    添加预览图

--- src/Statics/js/itdoc.js
+++ src/Statics/js/itdoc.js
@@ -1,7 +1,7 @@
 function c(){
     var r= new FileReader();
     f=document.getElementById('file').files[0];
-
+console.log(f);
     r.readAsDataURL(f);
     r.onload=function (e) {
         document.getElementById('show').src=this.result;
