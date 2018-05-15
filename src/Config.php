828794421c0e5641ed275af9e1e1908f97d6c877
huangxianan <hzhxn@tairanchina.com>
2018-05-15 10:30:47 +0800

    自定义图片名称bug修复

--- src/Config.php
+++ src/Config.php
@@ -12,6 +12,7 @@ return [
     //1 = GIF，2 = JPG，3 = PNG，4 = SWF，5 = PSD，6 = BMP，7 = TIFF(intel byte order)，8 = TIFF(motorola byte order)，
     //9 = JPC，10 = JP2，11 = JPX，12 = JB2，13 = SWC，14 = IFF，15 = WBMP，16 = XBM
     'supportImgType' => [
+        '1' => 'gif',
         '2' => 'jpg',
         '3' => 'png'
     ],
